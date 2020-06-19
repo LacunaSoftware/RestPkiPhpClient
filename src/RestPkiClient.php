b2f06ca4dda6db97b37dae49b7c05a088d2049fc
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2020-06-19 14:05:24 -0300

    Incremented version.

--- src/RestPkiClient.php
+++ src/RestPkiClient.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\ResponseInterface;
  */
 class RestPkiClient
 {
-    const LIB_VERSION = '2.4.0';
+    const LIB_VERSION = '2.5.0';
 
     public $multipartUploadThreshold = 5242880; // 5 MB
     public $restPkiVersion;
