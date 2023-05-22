fd3c21b5e904f0a3ef788bc2aae043a4421f8d5d
Danilo Bispo <danilob@lacunasoftware.com>
2023-05-22 15:49:25 -0300

    Updated library version in header

--- src/RestPkiClient.php
+++ src/RestPkiClient.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\ResponseInterface;
  */
 class RestPkiClient
 {
-    const LIB_VERSION = '2.9.2';
+    const LIB_VERSION = '2.9.9';
 
     public $multipartUploadThreshold = 5242880; // 5 MB
     public $restPkiVersion;
