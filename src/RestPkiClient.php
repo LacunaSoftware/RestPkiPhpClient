8a1eaf445edcc29ea6547d26d8478c2b873a1773
Danilo Bispo <danilobispogalvao@gmail.com>
2023-05-26 09:52:01 -0300

    Changed lib version to another minor

--- src/RestPkiClient.php
+++ src/RestPkiClient.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\ResponseInterface;
  */
 class RestPkiClient
 {
-    const LIB_VERSION = '2.9.9';
+    const LIB_VERSION = '2.9.10';
 
     public $multipartUploadThreshold = 5242880; // 5 MB
     public $restPkiVersion;
